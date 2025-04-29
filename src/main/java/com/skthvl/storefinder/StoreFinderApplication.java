0c5601143bc094679b1673f3a565873444742fa9
Sakthivel Balasubramaniam <ideamatex@gmail.com>
2025-04-29 12:06:55 +0530

    fix checkstyle

--- src/main/java/com/skthvl/storefinder/StoreFinderApplication.java
+++ src/main/java/com/skthvl/storefinder/StoreFinderApplication.java
@@ -1,7 +1,5 @@
 package com.skthvl.storefinder;
 
-import jakarta.annotation.PostConstruct;
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,13 +16,5 @@ public class StoreFinderApplication {
    */
   public static void main(final String[] args) {
     SpringApplication.run(StoreFinderApplication.class, args);
-  }
-
-  @PostConstruct
-  public void init() {
-    final var envAppTimeZone = System.getenv("APP_TIMEZONE");
-    final var timeZone =
-        envAppTimeZone != null ? TimeZone.getTimeZone(envAppTimeZone) : TimeZone.getDefault();
-    TimeZone.setDefault(timeZone);
   }
 }
